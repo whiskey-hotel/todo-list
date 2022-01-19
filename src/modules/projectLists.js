f7f6c1df550e440b7194a160b4f7d5ecc083ac7c
Justin Thornton <jathom15@gmail.com>
2022-01-18 23:08:59 -0600

    Dynamically add projects

--- src/modules/projectLists.js
+++ src/modules/projectLists.js
@@ -4,8 +4,9 @@ const projects = (name) => {
 	const getName = () => name;
 
 	const create = function () {
-		const newProject = newElement("div", "project-list", `${this.name}-list`);
-		const newListTitle = newElement("h3", "project-title", ...Array(1), `${this.name}`);
+		const modifiedNameForID = name.replace(/\s/g, "");
+		const newProject = newElement("div", "project-list", `${modifiedNameForID}-list`);
+		const newListTitle = newElement("h3", "project-title", ...Array(1), `${name}`);
 		const numberOfTasks = newElement("span", "number-of-tasks", ...Array(1), "0");
 
 		newProject.appendChild(newListTitle);
@@ -31,10 +32,10 @@ const tasks = (task, day, time, notes, URL) => {
 };
 
 function home() {
-	const projectsDiv = newElement("div", "projects-container");
+	const projectsContainer = newElement("div", "projects-container");
+	const projectsDiv = newElement("div", ...Array(1), "projects-div");
 	const projectHeader = newElement("div", "header");
 	const projectTitle = newElement("h1", "title", ...Array(1), "My Projects");
-	const projectListDiv = newElement("div", "project-list-div");
 	const mainList = newElement("div", "project-list", "all-list");
 	const listTitle = newElement("h3", "project-title", ...Array(1), "All");
 	const numberOfTasks = newElement("span", "number-of-tasks", ...Array(1), "0");
@@ -48,13 +49,13 @@ function home() {
 	const addProjectIcon = newElement("span", "material-icons-outlined", "add-project-icon", "add_circle_outline");
 	const addProject = newElement("span", ...Array(1), "add-project", "Add Project");
 
-	projectsDiv.appendChild(projectHeader);
-	projectsDiv.appendChild(projectTitle);
-	projectsDiv.appendChild(projectListDiv);
+	projectsContainer.appendChild(projectHeader);
+	projectsContainer.appendChild(projectsDiv);
+	projectHeader.appendChild(projectTitle);
 	projectsDiv.appendChild(mainList);
 	mainList.appendChild(listTitle);
 	mainList.appendChild(numberOfTasks);
-	projectsDiv.appendChild(addProjectDiv);
+	projectsContainer.appendChild(addProjectDiv);
 	addProjectDiv.appendChild(addProjectIcon);
 	addProjectDiv.appendChild(addProject);
 
@@ -66,12 +67,12 @@ function home() {
 		sendToBody(displayNewProjectWindow());
 	});
 
-	return { projectsDiv, taskDiv };
+	return { projectsContainer, taskDiv };
 }
 
 function displayNewProjectWindow() {
-	const newProjectContainer = newElement("div", ...Array(1), "new-project-container");
-	const newProjectDiv = newElement("div", ...Array(1), "new-project-div");
+	const newProjectContainer = newElement("div", ...Array(1), "new-project-form-container");
+	const newProjectDiv = newElement("div", ...Array(1), "new-project-form-div");
 	const title = newElement("h2", "pop-up-window-title", ...Array(1), "New Project");
 	const projectNameInputLabel = newElement("label", "form-labels", ...Array(1), "Name:");
 	const projectNameInput = newElement("input", "form-input", "project-name-input");
@@ -81,7 +82,7 @@ function displayNewProjectWindow() {
 	const cancelButton = newElement("button", "button", "project-cancel-button", "Cancel");
 	const submitButton = newElement("input", "button", "project-submit-button");
 	submitButton.type = "submit";
-    submitButton.value = "OK";
+	submitButton.value = "OK";
 	submitButton.setAttribute("for", "project-name-input");
 
 	newProjectContainer.appendChild(newProjectDiv);
@@ -92,8 +93,23 @@ function displayNewProjectWindow() {
 	buttonSelectorDiv.appendChild(cancelButton);
 	buttonSelectorDiv.appendChild(submitButton);
 
+	const closeWindow = (element) => {
+		document.body.removeChild(element);
+	};
+
 	cancelButton.addEventListener("click", function () {
-		document.body.removeChild(newProjectContainer);
+		closeWindow(newProjectContainer);
+	});
+
+	submitButton.addEventListener("click", function () {
+		// const projectsDiv = home().projectsContainer.childNodes[1];
+		const projectsDiv = document.getElementById('projects-div');
+		let projectNameValue = projectNameInput.value;
+		let instantiateProjectObject = projects(projectNameValue);
+		let newProject = instantiateProjectObject.create();
+		projectsDiv.appendChild(newProject);
+		closeWindow(newProjectContainer);
+		
 	});
 
 	return newProjectContainer;
