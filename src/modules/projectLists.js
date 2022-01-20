a152df24ad7de04ae10016f304dac37e1fbb7b5b
Justin Thornton <jathom15@gmail.com>
2022-01-19 18:46:13 -0600

    add task icon

--- src/modules/projectLists.js
+++ src/modules/projectLists.js
@@ -14,21 +14,19 @@ const projects = (name) => {
 
 		return newProject;
 	};
+    const updateNumberOfTasks = function()
+    {}
 
 	const update = function () {};
 	const del = function () {};
 
-	return { getName, create };
+	return { getName, create, updateNumberOfTasks };
 };
 
 const tasks = (task, day, time, notes, URL) => {
-	const getTask = () => task;
-	const getDay = () => day;
-	const getTime = () => time;
-	const getNotes = () => notes;
-	const getURL = () => URL;
+    const create = function(){}
 
-	return { getTask, getDay, getTime, getNotes, getURL };
+	return {  };
 };
 
 function home() {
@@ -40,14 +38,18 @@ function home() {
 	const listTitle = newElement("h3", "project-title", ...Array(1), "All");
 	const numberOfTasks = newElement("span", "number-of-tasks", ...Array(1), "0");
 
-	const taskDiv = newElement("div", "task-container");
+    const taskContainer = newElement("div", "task-container")
+	const taskDiv = newElement("div",...Array(1) ,"task-div");
 	const taskHeader = newElement("div", "header");
-	const taskTitle = newElement("h1", "title", ...Array(1), "All");
+	const taskTitle = newElement("h2", "title", ...Array(1), "All");
 	const taskListDiv = newElement("div", "task-list-div");
 
 	const addProjectDiv = newElement("div", ...Array(1), "add-project-div");
 	const addProjectIcon = newElement("span", "material-icons-outlined", "add-project-icon", "add_circle_outline");
 	const addProject = newElement("span", ...Array(1), "add-project", "Add Project");
+
+    const addTaskDiv = newElement("div", ...Array(1), "add-task-div");
+	const addTaskIcon = newElement("span", "material-icons-outlined", "add-task-icon", "add_circle");
 
 	projectsContainer.appendChild(projectHeader);
 	projectsContainer.appendChild(projectsDiv);
@@ -59,15 +61,18 @@ function home() {
 	addProjectDiv.appendChild(addProjectIcon);
 	addProjectDiv.appendChild(addProject);
 
+    taskContainer.appendChild(taskDiv)
 	taskDiv.appendChild(taskHeader);
-	taskDiv.appendChild(taskTitle);
+	taskHeader.appendChild(taskTitle);
 	taskDiv.appendChild(taskListDiv);
+    taskContainer.appendChild(addTaskDiv);
+    addTaskDiv.appendChild(addTaskIcon);
 
 	addProjectDiv.addEventListener("click", function () {
 		sendToBody(displayNewProjectWindow());
 	});
 
-	return { projectsContainer, taskDiv };
+	return { projectsContainer, taskContainer };
 }
 
 function displayNewProjectWindow() {
@@ -113,6 +118,62 @@ function displayNewProjectWindow() {
 	});
 
 	return newProjectContainer;
+}
+
+function displayNewTaskWindow(){
+    const newTaskContainer = newElement("div", ...Array(1), "new-task-form-container");
+	const newTaskDiv = newElement("div", ...Array(1), "new-task-form-div");
+	const title = newElement("h2", "pop-up-window-title", ...Array(1), "New Task");
+
+	const taskNameInputLabel = newElement("label", "form-labels", ...Array(1), "Task:");
+	const taskNameInput = newElement("input", "form-input", "task-name-input");
+	taskNameInput.type = "text";
+	taskNameInputLabel.setAttribute("for", "task-name-input");
+
+    const taskNotesInputLabel = newElement("label", "form-labels", ...Array(1), "Notes:");
+    const taskNotesInput = newElement("input", "form-input", "task-notes-input");
+    taskNotesInput.type = "text";
+    taskNotesInputLabel.setAttribute("for", "task-notes-input")
+
+    const date;
+    const time;
+
+	const buttonSelectorDiv = newElement("div", ...Array(1), "button-selector-div");
+	const cancelButton = newElement("button", "button", "task-cancel-button", "Cancel");
+	const submitButton = newElement("input", "button", "task-submit-button");
+	submitButton.type = "submit";
+	submitButton.value = "OK";
+	submitButton.setAttribute("for", "task-name-input");
+
+	newProjectContainer.appendChild(newProjectDiv);
+	newProjectDiv.appendChild(title);
+	newProjectDiv.appendChild(projectNameInputLabel);
+	newProjectDiv.appendChild(projectNameInput);
+	newProjectDiv.appendChild(buttonSelectorDiv);
+	buttonSelectorDiv.appendChild(cancelButton);
+	buttonSelectorDiv.appendChild(submitButton);
+
+	const closeWindow = (element) => {
+		document.body.removeChild(element);
+	};
+
+	cancelButton.addEventListener("click", function () {
+		closeWindow(newProjectContainer);
+	});
+
+	submitButton.addEventListener("click", function () {
+		// const projectsDiv = home().projectsContainer.childNodes[1];
+		const projectsDiv = document.getElementById('projects-div');
+		let projectNameValue = projectNameInput.value;
+		let instantiateProjectObject = projects(projectNameValue);
+		let newProject = instantiateProjectObject.create();
+		projectsDiv.appendChild(newProject);
+		closeWindow(newProjectContainer);
+		
+	});
+
+	return newProjectContainer;
+
 }
 
 export { home };
