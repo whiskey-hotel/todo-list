10f83e1659d4dd446c21d2044a29572c6d765e33
Justin Thornton <jathom15@gmail.com>
2022-02-08 21:11:46 -0600

    Remove DOM manipulation from projectHandler

--- src/modules/elementBuilder.js
+++ src/modules/elementBuilder.js
@@ -1,8 +1,7 @@
-import { newElement, sendToBody, removeAllChildNodes } from "./DOMController";
+import { newElement, sendToBody } from "./DOMController";
 import { newTask, displayNewTaskWindow, showHideCompletedTasks } from "./taskHandler";
-import { newProject, displayNewProjectWindow, displayAllProjectTasks } from "./projectHandler";
+import { displayAllProjectTasks, displaySelectedProjectTasks, removeAllTasks, createProject } from "./projectHandler";
 import { pageState } from "./storage";
-import { staticTaskCount } from "./taskCountTracking";
 import { projects } from "./objectFactory";
 
 function home(mainObj) {
@@ -64,10 +63,126 @@ function home(mainObj) {
 
 	let showCompleted = false;
 	completedDiv.addEventListener("click", function () {
-		showCompleted = showHideCompletedTasks(showCompleted)
+		showCompleted = showHideCompletedTasks(showCompleted);
 	});
 
 	return { projectsContainer, taskContainer };
+}
+
+function newProject(obj) {
+	const name = obj.projectName;
+	const storageKey = obj.key;
+	const numberOfTasks = obj.numberOfTasks;
+
+	const modifiedNameForID = name.replace(/\s/g, "");
+	const newProject = newElement("div", "project-list", `${modifiedNameForID}-list`);
+	newProject.setAttribute("data-value", storageKey);
+	const newListTitle = newElement("h3", "project-title", ...Array(1), `${name}`);
+	const numberOfTasksElement = newElement("span", "number-of-tasks", `${storageKey}-task-count`, `${numberOfTasks}`);
+
+	const moreInfoIcon = newElement("span", "material-icons-outlined", `info-icon-${storageKey}`, "info");
+	moreInfoIcon.classList.add("more-info-icon");
+	newProject.addEventListener("mouseover", function () {
+		moreInfoIcon.style.display = "inline-block";
+	});
+
+	newProject.addEventListener("mouseout", function () {
+		moreInfoIcon.style.display = "none";
+	});
+
+	moreInfoIcon.addEventListener("click", function () {
+		const dropDownDiv = dropDownOptionForProjects(storageKey);
+		newProject.appendChild(dropDownDiv);
+	});
+
+	newProject.addEventListener("click", function (e) {
+		displaySelectedProjectTasks(e);
+	});
+
+	newProject.appendChild(newListTitle);
+	newProject.appendChild(numberOfTasksElement);
+	newProject.appendChild(moreInfoIcon);
+
+	return newProject;
+}
+
+function dropDownOptionForProjects(storageKey) {
+	const dropDownDiv = newElement("div", "drop-down-content", "drop-down");
+	const updateButton = newElement("span", "drop-down-option", `task${storageKey}-update-button`, "Rename");
+	const deleteButton = newElement("span", "drop-down-option", `task${storageKey}-delete-button`, "Delete");
+	updateButton.setAttribute("data-value", storageKey);
+	deleteButton.setAttribute("data-value", storageKey);
+
+	let newProject = document.body.querySelector(`.project-list[data-value=${storageKey}]`);
+	let more = document.body.querySelector(`#info-icon-${storageKey}`);
+
+	/* This behavior is unexpected. looping through all storagekeys instead of just the one storage
+        key associated with the clicked element. error occurs in the console at every execution */
+	document.addEventListener("click", function (e) {
+		if (document.getElementById("drop-down") && !document.getElementById("drop-down").contains(e.target) && e.target != more) {
+			newProject.removeChild(document.getElementById("drop-down"));
+		}
+	});
+
+	updateButton.addEventListener("click", function () {
+		sendToBody(displayNewProjectWindow(storageKey));
+		newProject.removeChild(dropDownDiv);
+	});
+
+	deleteButton.addEventListener("click", function () {
+		const mainProjectsDiv = document.getElementById("projects-div");
+		const deletedProject = document.body.querySelector(`.project-list[data-value=${storageKey}`);
+		removeAllTasks(storageKey);
+		projects().deleteProject(storageKey);
+		mainProjectsDiv.removeChild(deletedProject);
+	});
+
+	dropDownDiv.appendChild(updateButton);
+	dropDownDiv.appendChild(deleteButton);
+
+	return dropDownDiv;
+}
+
+function displayNewProjectWindow(storageKey = null) {
+	const newProjectContainer = newElement("div", ...Array(1), "new-project-form-container");
+	const newProjectDiv = newElement("div", ...Array(1), "new-project-form-div");
+	const title = newElement("h2", "pop-up-window-title", ...Array(1), "New Project");
+	const error = newElement("div", ...Array(1), "project-name-error");
+	const projectForm = newElement("form", ...Array(1), "project-form");
+	const projectNameInputLabel = newElement("label", "form-labels", ...Array(1), "Name:");
+	const projectNameInput = newElement("input", "form-input", "project-name-input");
+	projectNameInput.type = "text";
+	projectNameInputLabel.setAttribute("for", "project-name-input");
+	const buttonSelectorDiv = newElement("div", ...Array(1), "button-selector-div");
+	const cancelButton = newElement("button", "button", "project-cancel-button", "Cancel");
+	const submitButton = newElement("input", "button", "project-submit-button");
+	submitButton.type = "submit";
+	submitButton.value = "OK";
+	submitButton.setAttribute("for", "project-name-input");
+
+	if (storageKey) {
+		projectNameInput.value = pageState.getStorage(`${storageKey}`)["projectName"];
+	}
+
+	newProjectContainer.appendChild(newProjectDiv);
+	newProjectDiv.appendChild(title);
+	newProjectDiv.appendChild(error);
+	newProjectDiv.appendChild(projectForm);
+	projectForm.appendChild(projectNameInputLabel);
+	projectForm.appendChild(projectNameInput);
+	projectForm.appendChild(buttonSelectorDiv);
+	projectForm.appendChild(cancelButton);
+	projectForm.appendChild(submitButton);
+
+	projectForm.addEventListener("submit", function (e) {
+		createProject(e, projectNameInput, storageKey);
+	});
+
+	cancelButton.addEventListener("click", function () {
+		closeWindow(newProjectContainer);
+	});
+
+	return newProjectContainer;
 }
 
 function recall(selectedProject = null, completed = null) {
@@ -94,4 +209,4 @@ function recall(selectedProject = null, completed = null) {
 	allProjectCount.childNodes[1].textContent = totalNumberOfTasks;
 }
 
-export { home, recall };
+export { home, recall, newProject, dropDownOptionForProjects, displayNewProjectWindow };
--- src/modules/projectHandler.js
+++ src/modules/projectHandler.js
@@ -1,8 +1,9 @@
-import { newElement, sendToBody, closeWindow, removeAllChildNodes } from "./DOMController";
+import { closeWindow, removeAllChildNodes } from "./DOMController";
 import { projects, tasks } from "./objectFactory";
 import { pageState } from "./storage";
 import { updateDOMForDeletingTask } from "./taskCountTracking";
 import { newTask } from "./taskHandler";
+import { newProject } from "./elementBuilder";
 
 
 function displayAllProjectTasks(e) {
@@ -26,95 +27,24 @@ function displayAllProjectTasks(e) {
 	}
 }
 
+function displaySelectedProjectTasks(e){
+	let dataValue = e.target.dataset.value;
+	let projectObj = pageState.getStorage(dataValue);
+	if (projectObj) {
+		const parent = document.getElementById("main-task-div");
+		removeAllChildNodes(parent);
+		const title = document.getElementById("project-title-for-task-list");
+		title.textContent = projectObj.projectName;
 
-function newProject(obj) {
-	const name = obj.projectName;
-	const storageKey = obj.key;
-	const numberOfTasks = obj.numberOfTasks;
-
-	const modifiedNameForID = name.replace(/\s/g, "");
-	const newProject = newElement("div", "project-list", `${modifiedNameForID}-list`);
-	newProject.setAttribute("data-value", storageKey);
-	const newListTitle = newElement("h3", "project-title", ...Array(1), `${name}`);
-	const numberOfTasksElement = newElement("span", "number-of-tasks", `${storageKey}-task-count`, `${numberOfTasks}`);
-
-	const moreInfoIcon = newElement("span", "material-icons-outlined", `info-icon-${storageKey}`, "info");
-	moreInfoIcon.classList.add("more-info-icon");
-	newProject.addEventListener("mouseover", function () {
-		moreInfoIcon.style.display = "inline-block";
-	});
-
-	newProject.addEventListener("mouseout", function () {
-		moreInfoIcon.style.display = "none";
-	});
-
-	moreInfoIcon.addEventListener("click", function () {
-		const dropDownDiv = dropDownOption(storageKey);
-		newProject.appendChild(dropDownDiv);
-	});
-
-	newProject.addEventListener("click", function (e) {
-		let dataValue = e.target.dataset.value;
-		let projectObj = pageState.getStorage(dataValue);
-		if (projectObj) {
-			const parent = document.getElementById("main-task-div");
-			removeAllChildNodes(parent);
-			const title = document.getElementById("project-title-for-task-list");
-			title.textContent = projectObj.projectName;
-
-			for (let i = 0; localStorage.key(i); i++) {
-				let storageKey = localStorage.key(i);
-				let storageObject = pageState.getStorage(storageKey);
-				if (storageObject.type == "task" && storageObject.projectKey == projectObj.key) {
-					let restoredTask = newTask(storageObject);
-					parent.appendChild(restoredTask);
-				}
+		for (let i = 0; localStorage.key(i); i++) {
+			let storageKey = localStorage.key(i);
+			let storageObject = pageState.getStorage(storageKey);
+			if (storageObject.type == "task" && storageObject.projectKey == projectObj.key) {
+				let restoredTask = newTask(storageObject);
+				parent.appendChild(restoredTask);
 			}
 		}
-	});
-
-	newProject.appendChild(newListTitle);
-	newProject.appendChild(numberOfTasksElement);
-	newProject.appendChild(moreInfoIcon);
-
-	return newProject;
-}
-
-function dropDownOption(storageKey) {
-	const dropDownDiv = newElement("div", "drop-down-content", "drop-down");
-	const updateButton = newElement("span", "drop-down-option", `task${storageKey}-update-button`, "Rename");
-	const deleteButton = newElement("span", "drop-down-option", `task${storageKey}-delete-button`, "Delete");
-	updateButton.setAttribute("data-value", storageKey);
-	deleteButton.setAttribute("data-value", storageKey);
-
-	let newProject = document.body.querySelector(`.project-list[data-value=${storageKey}]`);
-	let more = document.body.querySelector(`#info-icon-${storageKey}`);
-
-	/* This behavior is unexpected. looping through all storagekeys instead of just the one storage
-        key associated with the clicked element. error occurs in the console at every execution */
-	document.addEventListener("click", function (e) {
-		if (document.getElementById("drop-down") && !document.getElementById("drop-down").contains(e.target) && e.target != more) {
-			newProject.removeChild(document.getElementById("drop-down"));
-		}
-	});
-
-	updateButton.addEventListener("click", function () {
-		sendToBody(displayNewProjectWindow(storageKey));
-		newProject.removeChild(dropDownDiv);
-	});
-
-	deleteButton.addEventListener("click", function () {
-		const mainProjectsDiv = document.getElementById("projects-div");
-		const deletedProject = document.body.querySelector(`.project-list[data-value=${storageKey}`);
-		removeAllTasks(storageKey);
-		projects().deleteProject(storageKey);
-		mainProjectsDiv.removeChild(deletedProject);
-	});
-
-	dropDownDiv.appendChild(updateButton);
-	dropDownDiv.appendChild(deleteButton);
-
-	return dropDownDiv;
+	}
 }
 
 function removeAllTasks(storageKey) {
@@ -133,72 +63,42 @@ function removeAllTasks(storageKey) {
 	}
 }
 
-function displayNewProjectWindow(storageKey = null) {
-	const newProjectContainer = newElement("div", ...Array(1), "new-project-form-container");
-	const newProjectDiv = newElement("div", ...Array(1), "new-project-form-div");
-	const title = newElement("h2", "pop-up-window-title", ...Array(1), "New Project");
-	const error = newElement("div", ...Array(1), "project-name-error");
-	const projectForm = newElement("form", ...Array(1), "project-form");
-	const projectNameInputLabel = newElement("label", "form-labels", ...Array(1), "Name:");
-	const projectNameInput = newElement("input", "form-input", "project-name-input");
-	projectNameInput.type = "text";
-	projectNameInputLabel.setAttribute("for", "project-name-input");
-	const buttonSelectorDiv = newElement("div", ...Array(1), "button-selector-div");
-	const cancelButton = newElement("button", "button", "project-cancel-button", "Cancel");
-	const submitButton = newElement("input", "button", "project-submit-button");
-	submitButton.type = "submit";
-	submitButton.value = "OK";
-	submitButton.setAttribute("for", "project-name-input");
-
-	if (storageKey) {
-		projectNameInput.value = pageState.getStorage(`${storageKey}`)["projectName"];
+function projectValidation(projectNameValue){
+	let errorArray = []
+	if (projectNameValue.toLowerCase() == "all") {
+		errorArray.push("Cannot use 'All' as a project name");
 	}
-
-	newProjectContainer.appendChild(newProjectDiv);
-	newProjectDiv.appendChild(title);
-	newProjectDiv.appendChild(error);
-	newProjectDiv.appendChild(projectForm);
-	projectForm.appendChild(projectNameInputLabel);
-	projectForm.appendChild(projectNameInput);
-	projectForm.appendChild(buttonSelectorDiv);
-	projectForm.appendChild(cancelButton);
-	projectForm.appendChild(submitButton);
-
-	projectForm.addEventListener("submit", (e) => {
-		const errorField = document.getElementById("project-name-error");
-		const projectsDiv = document.getElementById("projects-div");
-
-		let messages = [];
-		let projectNameValue = projectNameInput.value;
-		if (projectNameValue.toLowerCase() == "all") {
-			messages.push("Cannot use 'All' as a project name");
-		}
-		if (projectNameValue === "" || projectNameValue == null) {
-			messages.push("Name is required");
-		}
-		if (messages.length > 0) {
-			e.preventDefault();
-			errorField.textContent = messages.join(", ");
-		} else {
-			let instantiateProjectObject = projects(projectNameValue);
-			if (storageKey) {
-				const updateProjectDiv = document.body.querySelector(`.project-list[data-value=${storageKey}]`);
-				updateProjectDiv.childNodes[0].textContent = projectNameValue;
-				instantiateProjectObject.update(storageKey, projectNameValue);
-			} else {
-				let newProjectObject = instantiateProjectObject.create();
-				let newElement = newProject(newProjectObject);
-				projectsDiv.appendChild(newElement);
-			}
-
-			closeWindow(newProjectContainer);
-		}
-	});
-
-	cancelButton.addEventListener("click", function () {
-		closeWindow(newProjectContainer);
-	});
-
-	return newProjectContainer;
+	if (projectNameValue === "" || projectNameValue == null) {
+		errorArray.push("Name is required");
+	}
+	return errorArray
 }
-export { newProject, displayNewProjectWindow, displayAllProjectTasks };
+
+function createProject (e, projectNameInput, storageKey) {
+	const errorField = document.getElementById("project-name-error");
+	const projectsDiv = document.getElementById("projects-div");
+	let projectNameValue = projectNameInput.value;
+
+	let messages = projectValidation(projectNameValue);
+	
+	if (messages.length > 0) {
+		e.preventDefault();
+		errorField.textContent = messages.join(", ");
+	} else {
+		let instantiateProjectObject = projects(projectNameValue);
+		if (storageKey) {
+			const updateProjectDiv = document.body.querySelector(`.project-list[data-value=${storageKey}]`);
+			updateProjectDiv.childNodes[0].textContent = projectNameValue;
+			instantiateProjectObject.update(storageKey, projectNameValue);
+		} else {
+			let newProjectObject = instantiateProjectObject.create();
+			let newElement = newProject(newProjectObject);
+			projectsDiv.appendChild(newElement);
+		}
+
+		closeWindow(newProjectContainer);
+	}
+}
+
+
+export { displayAllProjectTasks, displaySelectedProjectTasks, removeAllTasks, createProject };
